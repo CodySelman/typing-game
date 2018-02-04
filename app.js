c9baaa9625e7b802cbfbce7ffd989e93eab66230
Cody Selman <codyleeselman@gmail.com>
2018-02-04 14:47:14 -0500

    Basic scrolling effect achieved.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 // challengeWords[] has been moved to its own file for organization purposes
 const  playerInputForm = document.getElementById('userInput');
 const currentWordForm = document.getElementById('current-word');
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
 
 // Event Listeners
 
@@ -11,6 +13,7 @@ playerInputForm.addEventListener('input', correctAnswer);
 // Initialize
 
 instantiateWord();
+scrollText();
 
 // Functions
 
@@ -32,5 +35,14 @@ function correctAnswer(e){
 }
 
 function scrollText(){
-
+    let pos = -500;
+    var id = setInterval(scroll, 10);
+    function scroll(){
+        if (pos >= windowWidth){
+            clearInterval(id);
+        } else {
+            pos += 5;
+            currentWordForm.style.left = pos + 'px';
+        }
+    }
 }
