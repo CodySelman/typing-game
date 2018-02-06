fc5f9ed45f33d39ce3494412ced115b431275e86
Cody Selman <codyleeselman@gmail.com>
2018-02-05 21:03:19 -0500

    Welcome Screen fixed. Game over screen needs to be fixed.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const body = document.getElementById('body');
-gameStart();
+// gameStart();
+welcomeScreen();
 
 // Variables
 
@@ -8,6 +9,7 @@ const playerInput = document.getElementById('playerInput');
 const playerInputForm = document.getElementById('playerInputForm');
 const scoreText = document.getElementById('score');
 const livesText = document.getElementById('lives');
+let isRunning = 'false';
 
 const columnList = ['col1', 'col2', 'col3', 'col4', 'col5'];
 const scrollInterval = setInterval(scrollText, 10);
@@ -23,10 +25,11 @@ let fallSpeed = 1;
 
 playerInput.addEventListener('input', checkAnswer);
 playerInputForm.addEventListener('submit', noRefresh);
+playerInputForm.addEventListener('submit', gameStart);
 
 // Initialize
 
-instantiateWord();
+// instantiateWord();
 
 // Functions
 
@@ -56,8 +59,9 @@ function instantiateWord() {
 function checkAnswer(e){
     e.preventDefault();
     const currentWords = document.getElementsByClassName('currentWords');
+    const scoreText = document.getElementById('score')
     for(i = 0; i <= (currentWords.length - 1); i += 1){
-        if (currentWords[i] && playerInput.value === currentWords[i].innerHTML){
+        if (currentWords[i] && playerInput.value === currentWords[i].innerHTML && (isRunning === 'true')){
             currentWords[i].parentNode.removeChild(currentWords[i]);
             playerInput.value = '';
             score += 1;
@@ -79,6 +83,7 @@ function removeFlash(){
 
 function scrollText(){
     const currentWords = document.getElementsByClassName('currentWords');
+    const livesText = document.getElementById('lives');
     for(i = 0; i <= (currentWords.length - 1); i += 1){
         let pos = currentWords[i].style.bottom.slice(0, -2);
         pos = Number(pos);
@@ -101,14 +106,60 @@ function scrollText(){
 }
 
 function welcomeScreen(){
-    // function that loads welcome screen upon page load
+    const h1 = document.createElement('h1');
+    h1.id = 'heading';
+    h1.classList.add('text-green', 'd-flex', 'justify-content-center', 'p-4', 'm-4');
+    body.appendChild(h1);
+    h1Text = document.createTextNode('Word Rain');
+    h1.appendChild(h1Text);
+    const h2 = document.createElement('h2');
+    h2.id = 'subheading';
+    h2.classList.add('text-green', 'd-flex', 'justify-content-center', 'p-4', 'm-4');
+    body.appendChild(h2);
+    h2Text = document.createTextNode('Press Enter to Play');
+    h2.appendChild(h2Text);
+
+    const footer = document.createElement('footer');
+    footer.classList.add('h-20');
+    body.appendChild(footer);
+    const form = document.createElement('form');
+    form.id = 'playerInputForm';
+    form.classList.add('inputForm', 'w-100', 'd-flex', 'justify-content-center');
+    form.autocomplete= 'off';
+    textInput = document.createElement('input');
+    textInput.type = 'text';
+    textInput.id = 'playerInput';
+    textInput.classList.add('display-4', 'bg-dark', 'text-green', 'w-100', 'text-center');
+    textInput.autofocus = 'true';
+    footer.appendChild(form);
+    form.appendChild(textInput);
 }
 
 function gameOver(){
-    // function showing game over screen upon losing and prompts for retry
+    const h1 = document.createElement('h1');
+    h1.id = 'heading';
+    h1.classList.add('text-green', 'd-flex', 'justify-content-center', 'p-4', 'm-4');
+    body.appendChild(h1);
+    h1Text = document.createTextNode('Game Over');
+    h1.appendChild(h1Text);
+    const h2 = document.createElement('h2');
+    h2.id = 'subheading';
+    h2.classList.add('text-green', 'd-flex', 'justify-content-center', 'p-4', 'm-4');
+    body.appendChild(h2);
+    h2Text = document.createTextNode('Press Enter to Restart');
+    h2.appendChild(h2Text);
+
+    playerInputForm.addEventListener('submit', gameStart);
 }
 
 function gameStart(){
+    playerInputForm.removeEventListener('submit', gameStart);
+
+    const h1 = document.getElementById('heading');
+    h1.parentNode.removeChild(h1);
+    const h2 = document.getElementById('subheading');
+    h2.parentNode.removeChild(h2);
+
     const main = document.createElement('main');
     main.id = 'main';
     main.classList.add('h-70', 'row');
@@ -129,26 +180,17 @@ function gameStart(){
     const pScore = document.createElement('p');
     pLives.id = 'lives';
     pScore.id = 'score';
+    pLives.classList.add('infoText', 'm-1');
+    pScore.classList.add('infoText', 'm-1');
     const col0 = document.getElementById('col0');
     col0.appendChild(pLives);
-    col0.appendChild(pScore);
+    const col6 = document.getElementById('col6');
+    col6.appendChild(pScore);
     pLivesText = document.createTextNode('Lives: 3');
     pScoreText = document.createTextNode('Score: 0');
     pLives.appendChild(pLivesText);
     pScore.appendChild(pScoreText);
 
-    const footer = document.createElement('footer');
-    footer.classList.add('h-20');
-    body.appendChild(footer);
-    const form = document.createElement('form');
-    form.id = 'playerInputForm';
-    form.classList.add('inputForm', 'w-100', 'd-flex', 'justify-content-center');
-    form.autocomplete= 'off';
-    textInput = document.createElement('input');
-    textInput.type = 'text';
-    textInput.id = 'playerInput';
-    textInput.classList.add('display-4', 'bg-dark', 'text-green', 'w-100', 'text-center');
-    textInput.autofocus = 'true';
-    footer.appendChild(form);
-    form.appendChild(textInput);
+    isRunning = 'true';
+    instantiateWord();
 }
